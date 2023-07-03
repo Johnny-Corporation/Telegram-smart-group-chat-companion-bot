1abbea49b668e2d1b7c4adddf120b5c786cbd5b8
MichaeLRoman <romanbanenko1@gmail.com>
2023-07-03 21:44:46 +0300

    hi

--- GPT_FUNCS.py
+++ GPT_FUNCS.py
@@ -1,22 +1,6 @@
 import os
 import openai
 
-
-from dotenv import load_dotenv
-from os import environ, getenv
-
-load_dotenv(".env")
-bot_token = environ.get("BOT_API_TOKEN")
-openAI_api_key = environ.get("OPENAI_API_KEY")
-developer_chat_IDs = environ.get("DEVELOPER_CHAT_IDS")
-
-openai_organization = environ.get("OPENAI_ORGANIZATION")
-model = "gpt-3.5-turbo"               #------------------------------------------
-#print(openai.Model.list())
-
-
-
-
 def question_to_bot(gpt_token,organization_token,message):
 
     message_to_gpt = ''
