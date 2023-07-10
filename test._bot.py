dfc6ac2f7005e4084cc98eaf4392864a0a998a52
MichaeLRoman <romanbanenko1@gmail.com>
2023-07-10 20:13:08 +0300

    /start key_error solved

--- test._bot.py
+++ test._bot.py
@@ -4,25 +4,32 @@ from telebot import types
 bot_token = "5918531073:AAF1q4o2WIkBuee4CTSc95XIWexUox9-Xw4"
 bot = telebot.TeleBot(bot_token)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    markup = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text="Нажми меня", callback_data='b')
-    button1 = types.InlineKeyboardButton(text="Нажми меня1", callback_data='a')
-    button2 = types.InlineKeyboardButton(text="Нажми меня2", callback_data='c')
-    markup.add(button)
-    markup.add(button1)
-    markup.add(button2)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку:", reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call: call.data == 'a' or call.data == 'b')
-def action(call):
-    bot.send_message(call.message.chat.id, "Вы нажали на кнопку!")
+# @bot.message_handler()
+# def echo_all(message):
+#     chat_type = message.chat.type  # Получаем тип чата
 
-@bot.callback_query_handler(func=lambda call: call.data == 'c')
-def action(call):
-    bot.send_message(call.message.chat.id, "Вы нажали на кнопку!1")
+#     if chat_type == "private":
+#         bot.reply_to(message, "Вы написали мне в личные сообщения.")
+#     elif chat_type in ["group", "supergroup"]:
+#         bot.reply_to(message, "Вы написали в групповом чате.")
+#     else:
+#         bot.reply_to(message, "Я не уверен, где вы мне написали.")
+
+
+@bot.message_handler(
+        func=lambda message: message.chat.type == 'private'
+)
+def handle_private_message(message):
+    # Здесь ваш код для обработки личных сообщений
+    bot.reply_to(message, "Это личное сообщение!")
+
+
+@bot.message_handler(
+    func=lambda message: message.chat.type in ["group", "supergroup"],
+)
+def echo_priv(message):
+    bot.reply_to(message, "Ты в группе")
+
 
 bot.polling()
-
-
