0ce62d179cde2793cd6fb72bd54f0cc54d546e78
Matvey Rotte <matveyrotte@gmail.com>
2023-09-05 22:48:06 +0300

    Variety/creativity changing always declined bug fixed

--- commands/reply_handlers/settings_bot_answers.py
+++ commands/reply_handlers/settings_bot_answers.py
@@ -47,7 +47,7 @@ def set_sphere_reply_handler(inner_message):
 def set_presence_penalty_reply_handler(inner_message):
     language_code = groups[inner_message.chat.id].lang_code
     try:
-        val = int(inner_message.text)
+        val = float(inner_message.text)
     except ValueError:
         bot.reply_to(inner_message, "❌")
         bot.send_message(
@@ -72,7 +72,7 @@ def set_presence_penalty_reply_handler(inner_message):
 def set_frequency_penalty_reply_handler(inner_message):
     language_code = groups[inner_message.chat.id].lang_code
     try:
-        val = int(inner_message.text)
+        val = float(inner_message.text)
     except ValueError:
         bot.reply_to(inner_message, "❌")
         bot.send_message(
@@ -88,32 +88,3 @@ def set_frequency_penalty_reply_handler(inner_message):
 
     groups[inner_message.chat.id].frequency_penalty = val
     bot.reply_to(inner_message, "✅")
-
-
-# --- reply handler for set temp memory size
-@error_handler
-def set_memory_size_reply_handler(inner_message):
-    language_code = groups[inner_message.chat.id].lang_code
-    try:
-        val = int(inner_message.text)
-    except ValueError:
-        bot.reply_to(inner_message, "❌")
-        bot.send_message(
-            inner_message.chat.id, templates[language_code]["set_memory_declined.txt"]
-        )
-        return
-    if val <= 0:
-        bot.reply_to(inner_message, "❌")
-        bot.send_message(
-            inner_message.chat.id, templates[language_code]["set_memory_declined.txt"]
-        )
-        return
-    
-    if groups[inner_message.chat.id].temporary_memory_size_limit >= val:
-
-        groups[inner_message.chat.id].change_memory_size = val
-        bot.reply_to(inner_message, "✅")
-
-    else:
-
-        bot.reply_to(inner_message, templates[language_code]["no_rights.txt"], parse_mode = "HTML")
