a33f63a7eb70140495c55a87fdf5f4fd7fb6392f
Matvey Rotte <matveyrotte@gmail.com>
2023-09-19 22:15:18 +0300

    Хватит тратить жизнь на это я устал

--- utils/internet_access.py
+++ utils/internet_access.py
@@ -34,14 +34,16 @@ def google(question: str) -> str:
 
 
 def remove_scripts_and_styles(html):
-    return re.sub(r'<(script|style).*?>.*?</\1>', '', html, flags=re.DOTALL)
+    return re.sub(r"<(script|style).*?>.*?</\1>", "", html, flags=re.DOTALL)
+
 
 def extract_text(input_string):
     result = ""
-    for i in re.findall(r'>\s*(.*?)(?:<|$)', input_string):
+    for i in re.findall(r">\s*(.*?)(?:<|$)", input_string):
         if i:
             result += i + "|"
     return result
+
 
 def read_from_link(link: str) -> str:
     logger.info(f'read_from_link function called, requested link: "{link}"')
@@ -50,7 +52,12 @@ def read_from_link(link: str) -> str:
         response = requests.get(link, timeout=5, headers=headers)
     except requests.exceptions.RequestException:
         return "Sever is not responding, cant read link"
-    all_visible_text = extract_text(remove_scripts_and_styles(response.content.decode()))
+    try:
+        all_visible_text = extract_text(
+            remove_scripts_and_styles(response.content.decode())
+        )
+    except:  # failed to decode in utf-8
+        all_visible_text = "no content on text on the page"
     all_text = all_visible_text[:5000]
 
     logger.info(f"Text parsed form link {link}:{all_text}")
