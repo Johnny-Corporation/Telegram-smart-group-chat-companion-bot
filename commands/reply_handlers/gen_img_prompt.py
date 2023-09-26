150328bbcd7570808790f95eda27a10b4859319e
Matvey Rotte <matveyrotte@gmail.com>
2023-09-26 12:29:22 +0300

    enhanced prompt for image fail handled

--- commands/reply_handlers/gen_img_prompt.py
+++ commands/reply_handlers/gen_img_prompt.py
@@ -6,7 +6,11 @@ from __main__ import *
 def img_prompt_reply_handler(inner_message):
     lang_code = groups[inner_message.chat.id].lang_code
     original_prompt = translate_text("en", inner_message.text, force=True)
-    enhanced_prompt = improve_img_gen_prompt(original_prompt)
+    bot.send_chat_action(inner_message.chat.id, "upload_photo")
+    try:
+        enhanced_prompt = improve_img_gen_prompt(original_prompt)
+    except:
+        enhanced_prompt = original_prompt
     sent_message = bot.send_message(
         inner_message.chat.id,
         translate_text(lang_code, "🎨🤖Generating image...")
