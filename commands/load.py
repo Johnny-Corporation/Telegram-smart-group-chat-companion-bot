007afffe5e3afbd344d2a6e652a9885f065cf0b4
MichaeLRoman <romanbanenko1@gmail.com>
2023-10-16 18:57:55 +0300

    choose lang in losd

--- commands/load.py
+++ commands/load.py
@@ -5,6 +5,10 @@ from __main__ import *
 @bot.message_handler(commands=["load", "help"], func=time_filter)
 @error_handler
 def help(message):
+
+    if groups[message.chat.id].activated == False:
+        change_language(message.chat.id)
+
     language_code = groups[message.chat.id].lang_code
     markup = load_buttons(
         types, groups, message.chat.id, language_code, owner_id=groups[message.chat.id].owner_id
